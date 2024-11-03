1c16477ebb140532897adc1323ffec9053cfd17e
Cami Barbon <camibarbon@gmail.com>
2024-11-03 12:29:47 -0300

    Carrusel con js, maquillaje social. Cambio de paleta a pedido de la artista

---
+++ js/mSocial.js
@@ -0,0 +1,31 @@
+const totalImages = 27; // Total de imágenes
+let currentIndex = 0; // Índice actual
+const carouselInner = document.getElementById('carouselImages');
+
+// Mostrar el modal al cargar la página
+$(document).ready(function() {
+    $('#carouselModal').modal('show');
+    loadImages(); // Cargar imágenes al abrir el modal
+    setInterval(changeImage, 30000); // Cambiar imágenes cada 30 segundos
+});
+
+// Función para cargar las imágenes en el carrusel
+function loadImages() {
+    carouselInner.innerHTML = ''; // Limpiar contenido previo
+
+    for (let i = 0; i < 27; i++) {
+        const srcIndex = (currentIndex + i) % totalImages + 1; // Rutas de imágenes
+        const isActive = i === 0 ? 'active' : ''; // Solo la primera imagen es activa
+        carouselInner.innerHTML += `
+            <div class="carousel-item ${isActive}">
+                <img src="../img/m-social/social-${srcIndex}.jpg" class="d-block w-100" alt="Imagen ${srcIndex}">
+            </div>
+        `;
+    }
+}
+
+// Función para alternar imágenes
+function changeImage() {
+    currentIndex = (currentIndex + 1) % totalImages; // Incrementar índice
+    loadImages(); // Cargar nuevas imágenes
+}
