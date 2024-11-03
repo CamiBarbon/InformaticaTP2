d705e403aa5e4369cb9df82a7dae39ea15b81e83
Cami Barbon <camibarbon@gmail.com>
2024-11-03 17:19:06 -0300

    Completas de contenido todas las páginas. Jquerys celular.

--- js/landing.js
+++ js/landing.js
@@ -11,13 +11,20 @@ setTimeout(() => {
 // Hacer que el logo aparezca 
 setTimeout(() => {
     logo.style.opacity = 1; 
+    // Hacer visibles los botones una vez que el logo es completamente opaco
+    buttonContainer.style.opacity = 1; 
+    buttons.forEach(button => {
+        button.style.opacity = 1; // Hacer que los botones sean visibles
+    });
 }, 1000);
 
 // Cuando el video termina
 video.addEventListener('ended', () => {
-    buttonContainer.style.opacity = 1; 
-
-    buttons.forEach(button => {
-        button.style.opacity = 1; // Hacer que los botones sean visibles
-    });
+    // Si los botones aún no son visibles, hacer que sean visibles
+    if (buttonContainer.style.opacity === '0' || buttonContainer.style.opacity === '') {
+        buttonContainer.style.opacity = 1; 
+        buttons.forEach(button => {
+            button.style.opacity = 1; // Hacer que los botones sean visibles
+        });
+    }
 });
